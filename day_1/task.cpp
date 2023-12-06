15f2b1bcf6468e264251cd80b38216df1acbdfee
Péter Dvorák <pdvorak370@gmail.com>
2023-12-06 18:22:51 +0100

    Sollved day1 part 2

--- day_1/task.cpp
+++ day_1/task.cpp
@@ -3,7 +3,7 @@
  * @author Péter Dvorák (pdvorak370@gmail.com)
  * @brief Source file for the main file.
  * @version 0.1
- * @date 2022-11-30
+ * @date 2023-12.01
  *
  * @copyright Copyright take it as you wish lol (c) 2022
  *
@@ -13,29 +13,25 @@
 #include <string>
 #include <vector>
 #include <fstream>
-#include <bits/stdc++.h>
-
 
 int getNumbersFromText(std::string &input_string);
 
 int countOutputVals(std::vector<int> &vec);
 
-const std::vector<std::string> words = {"one", "two","three","four","five","six","seven","eight","nine"};
+std::string replaceWords(std::string &input);
 
-int getNumberFromTextWithWords(std::string &input);
-
-
+const std::vector<std::string> words = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+const std::vector<std::string> changed_words = {"on1ne", "tw2wo", "thre3hree", "fou4our", "fiv5ive", "si6ix", "seve7even", "eigh8ight", "nin9ine"};
 
 int main()
 {
     std::ifstream file("./input.txt");
-    std::string input_string;   
+    std::string input_string;
     std::vector<int> int_vec;
     while (std::getline(file, input_string))
     {
-        // std::cout << input_string << std::endl;
-        int_vec.push_back(getNumbersFromText(input_string));
-        getNumberFromTextWithWords(input_string);
+        std::string i_str = replaceWords(input_string);
+        int_vec.push_back(getNumbersFromText(i_str));
     }
     std::cout << countOutputVals(int_vec) << std::endl;
     return 0;
@@ -45,100 +41,55 @@ int getNumbersFromText(std::string &input_string)
 {
     std::vector<int> numbers;
     std::string output_val = "";
-    for(const auto &ch : input_string)
+    for (const auto &ch : input_string)
     {
-        if(ch >= 48 && ch <= 57)
+        if (ch >= 48 && ch <= 57)
         {
             std::string s = "";
             s += ch;
             numbers.push_back(std::stoi(s));
         }
     }
-    if(numbers.size() == 1)
+    if (numbers.size() == 1)
     {
         output_val += std::to_string(numbers.at(0));
         output_val += std::to_string(numbers.at(0));
+        std::cout << output_val << std::endl;
         return std::stoi(output_val);
     }
     else
     {
         output_val += std::to_string(numbers.at(0));
         output_val += std::to_string(numbers.at(numbers.size() - 1));
+        std::cout << output_val << std::endl;
         return std::stoi(output_val);
     }
+    std::cout << output_val << std::endl;
     return std::stoi(output_val);
 }
 
 int countOutputVals(std::vector<int> &vec)
 {
     int output_val = 0;
-    for(const auto &number : vec)
+    for (const auto &number : vec)
         output_val += number;
     return output_val;
 }
 
-
-int getNumberFromTextWithWords(std::string &input)
+std::string replaceWords(std::string &input)
 {
-    int last_num_pos = 0, first_num_pos = INFINITY;
-    int last_word_pos = 0, first_word_pos = INFINITY;
-    int num_of_words = 0, num_of_nums = 0;
-    std::vector<int> numbers;
-    for(const auto &word : words)
+    int position = 0;
+    for (int i = 0; i < words.size(); i++)
     {
-        int found = input.find(word);
-        if (found != -1)
+        while (position != -1)
         {
-            std::cout << word << " found in " << found << std::endl;
-            num_of_words++;
-        }
-
-        if (last_word_pos < found)
-        {
-            last_word_pos = found + word.size() - 1;
-            std::cout << input << " word found: " << word << " " << "in pos " << last_word_pos << " word size " << word.size() << std::endl;
-        }
-        if(first_word_pos > found)
-        {
-            first_word_pos = found + word.size() - 1;
-        }
-    }
-    for(size_t i = 0; i < input.size(); i++)
-    {
-        if(input[i] >= 48 && input[i] <= 57)
-        {
-            num_of_nums++;
-            if (last_num_pos < i)
+            position = input.find(words[i]);
+            if (position != -1)
             {
-                last_num_pos = i;
-            }
-            if(first_num_pos > i)
-            {
-                first_num_pos = i;
+                input.replace(position, words[i].size(), changed_words[i]);
             }
         }
+        position = 0;
     }
-    std::string final_num = "";
-    if(num_of_nums == 1 && num_of_words == 0)
-    {   
-        final_num += input[last_num_pos] + input[last_num_pos];
-        return std::stoi(final_num);
-    }
-    else if(num_of_words == 1 && num_of_nums == 0)
-    {
-        final_num += input[last_word_pos] + input[last_word_pos];
-        return std::stoi(final_num);     
-    }
-    if(first_num_pos > first_word_pos)
-    {
-        final_num
-    }
-    else
-    {}
-    if(last_num_pos > last_word_pos)
-    {
-        final_num += 
-    }
-    else
-    {}
+    return input;
 }
